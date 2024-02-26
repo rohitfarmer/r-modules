f6bce660b8864fecad3709c1d901b8ace08ef409
Rohit Farmer <rohit.farmer@gmail.com>
2024-02-26 15:02:46 -0500

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# r-modules
-Collection of R Modules for Various Purposes
+# Collection of R Modules for Various Purposes
+
+The key idea of [`modules`](https://cran.r-project.org/web/packages/modules/vignettes/modulesInR.html) package in R is to provide a unit of source code which has it’s own scope. The main and most reliable infrastructure for such organizational units in the R ecosystem is a package. However, R packages are harder to create and maintain. Modules can be used as stand alone, ad-hoc substitutes for a package or as a sub-unit within a package. By using `modules` we can import functions from an external script in a cleaner way than sourcing an R script.
+
+
+## Search the local and online NCBI taxonomy database
+`taxonomy.R` module has functions to search a local database/table for taxonomy IDs given organism names and then use the taxonomy IDs to search for taxonomy levels. Or to search online NCBI taxonomy databases for a list of organisim names. 
+
+The local databases files/tables are available at [https://zenodo.org/records/10672196](https://zenodo.org/records/10672196) as `feather` files. 
+
+For the online search, the function uses [`taxize`](https://cran.r-project.org/package=taxize) package which will require [NCBI's E-utilities API key](https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/) in your R environment.
+
+Check out: 
+  - [https://books.ropensci.org/taxize/authentication.html](https://books.ropensci.org/taxize/authentication.html)
+  - [https://books.ropensci.org/taxize/](https://books.ropensci.org/taxize/)
+
+
+```R
+library(modules)
+
+# Load the module and create an object
+mtax <- modules::use(file.path("r-modules", "taxonomy.R"))
+
+# Carry out local database search
+tax_dat <- mtax$names_to_tax_local(orgnames, names_db = namesdb, rankedlineage_db = rankedlineage, verbose = TRUE)
+
+# Carry out online database search
+tax_dat <- names_to_tax_online(orgnames, verbose = FALSE)
+```
--- taxonomy.R
+++ taxonomy.R
@@ -1,26 +1,32 @@
 if (!require('doMC')) install.packages('doMC'); import(doMC)
-if (!require('parallel')) install.packages('parallel'); import(parallel)
+if (!require('dplyr')) install.packages('dplyr'); import(dplyr)
 if (!require('foreach')) install.packages('foreach'); import(foreach)
+if (!require('parallel')) install.packages('parallel'); import(parallel)
+if (!require('tibble')) install.packages('tibble'); import(tibble)
 
-names_to_taxonomy <- function(orgnames, names_db, rankedlineage_db){
-  df_out <- data.frame()
-  cores <- detectCores(all.tests = FALSE, logical = FALSE)
-  cores <- cores - 1
-  registerDoMC(cores)
-  df_out <- foreach(i = 1:length(orgnames), .combine = rbind) %dopar%{
-    tax <- orgnames[[i]]
-    cat(paste(i, tax, "\n"))
-    #taxid <- names_db %>% dplyr::filter(name_txt == tax) %>%
-      #pull(tax_id)
-    taxid <- names_db[names_db$name_txt == tax,][["tax_id"]]
-    
-    if(length(taxid) !=0){
-      #rankedlineage_db %>% dplyr::filter(tax_id == taxid) %>%
-        #dplyr::mutate("query_org_name" = tax)
-      temp_df <- rankedlineage_db[rankedlineage_db$tax_id == taxid,]
-      temp_df$query_org_name <- tax
-      temp_df
-    }
-  }  
-  return(df_out)
+names_to_tax_local <- function(orgnames, names_db, rankedlineage_db, verbose = FALSE){
+   #     df_out <- data.frame()
+        df_out <- tibble()
+        cores <- detectCores(all.tests = FALSE, logical = FALSE)
+        cores <- cores - 1
+        registerDoMC(cores)
+        #df_out <- foreach(i = 1:length(orgnames), .combine = rbind) %dopar%{
+        df_out <- foreach(i = 1:length(orgnames), .combine = bind_rows) %dopar%{
+                tax <- orgnames[[i]]
+                if(verbose){
+                        cat(paste(i, tax, "\n"))
+                }
+                taxid <- names_db %>% dplyr::filter(name_txt == tax) %>%
+                        dplyr::pull(tax_id)
+#                taxid <- names_db[names_db$name_txt == tax,][["tax_id"]]
+
+                if(length(taxid) !=0){
+                        rankedlineage_db %>% dplyr::filter(tax_id == taxid) %>%
+                                dplyr::mutate("query_org_name" = tax)
+#                temp_df <- rankedlineage_db[rankedlineage_db$tax_id == taxid,]
+#                temp_df$query_org_name <- tax
+#                temp_df
+                }
+        }  
+        return(df_out)
 }
